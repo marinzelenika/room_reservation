ef219948671895199c3870842446d9dbf82b1ee5
Marin Zelenika <marin.zelenika@ephzhb.ba>
2020-09-11 14:00:45 +0200

    11.9.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,14 +11,13 @@ import '../css/app.css';
 var $ = require('jquery');
 require('bootstrap');
 
- // import the function from greet.js (the .js extension is optional)
-     // ./ (or ../) means to look for a local file
-   //      var greet = require('./greet');
 
-// $(document).ready(function() {
-//         $('body').prepend('<h1>'+greet('john')+'</h1>');
- //    });
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+async function showDays() {
+    var date1 = document.getElementById('date1');
+    var date2 = document.getElementById('date2');
+    document.getElementsByClassName('app').innerHTML = (date2 - date1) / 1000;
+}
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
